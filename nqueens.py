f16f8c1ee227c555a605cc90662db6ed3a243490
Roshni Gupta <roshni.g@turing.com>
2025-03-17 15:25:27 +0530

    Initialize the Backtracking Function

--- nqueens.py
+++ nqueens.py
@@ -1,3 +1,9 @@
 def solveNQueens(n):
     def is_safe(row, col):
         return col not in cols and (row - col) not in diagonal1 and (row + col) not in diagonal2
+    
+    def solve_n_queens(row):
+    if row == n:
+        board = ["".join(row) for row in chessboard]
+        solutions.append(board)
+        return
